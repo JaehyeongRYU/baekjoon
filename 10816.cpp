a2348fbc223ec8f52cbaa110c4db64f885b9c59e
JaehyeongRyu <dbwogud227@naver.com>
2020-06-28 17:40:16 +0900

    sunday

--- 10816.cpp
+++ 10816.cpp
@@ -3,34 +3,26 @@
 #include <vector>
 
 using namespace std;
-
-
 int main() {
 
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	int n, m;
+	int n,m;
 	cin >> n;
 	vector<int>v1(n);
+	
 	for (int i = 0; i < n; i++) {
 		cin >> v1[i];
 	}
 	sort(v1.begin(), v1.end());
-	cin >> m;
-	vector<int>v2(m);
-	for (int i = 0; i < m; i++) {
-		cin >> v2[i];
-	}
 
+	cin >> m;
 	for (int i = 0; i < m; i++) {
-		if (binary_search(v1.begin(), v1.end(), v2[i]) == true) {
-			cout << "1" << ' ';
-		}
-		else
-			cout << "0" << ' ';
+		int num;
+		cin >> num;
+		cout << upper_bound(v1.begin(), v1.end(), num) - lower_bound(v1.begin(), v1.end(), num) << " ";
 	}
-	
+	cout << "\n";
 
 	return 0;
 }
